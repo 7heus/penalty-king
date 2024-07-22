32bd2627aa6f28a347d3b0214c6a21447260e8f3
Theus <matheusgoias@hotmail.com>
2024-07-22 12:04:48 +0100

    Added new JS files game and ball, implemented some code

--- js/ball.js
+++ js/ball.js
@@ -1,3 +1,36 @@
 class Ball {
-  constructor() {}
+  constructor(gameScreen, imgSrc) {
+    this.imgSrc = imgSrc;
+
+    this.direction;
+    this.gameScreen = gameScreen;
+
+    this.width = 50;
+    this.height = 50;
+  }
+
+  createBall() {
+    const ball = document.createElement("img");
+    ball.setAttribute("src", this.imgSrc);
+    ball.style.width = `${this.width}px`;
+    ball.style.height = `${this.height}px`;
+    ball.setAttribute("class", "football");
+    ball.style.scale = "100%";
+
+    this.gameScreen.appendChild(ball);
+  }
+
+  removeBall(ball) {
+    ball.remove();
+  }
+
+  move(direction) {
+    const ball = document.querySelector(".football");
+    const left = ball.style.left;
+    const top = ball.style.top;
+    let scaling = parseInt(ball.style.scale.slice(0, ball.style.scale.length - 1)); // prettier-ignore
+    switch (direction) {
+      case "ArrowLeft":
+    }
+  }
 }
--- js/game.js
+++ js/game.js
@@ -1,3 +1,20 @@
 class Game {
-  constructor() {}
+  constructor() {
+    this.gameScreen = document.querySelector("#game-screen");
+    this.penaltiesScoreboard = document.querySelector("#penalties-scoreboard");
+    this.attemptsCounter = document.querySelector("#attempts-counter");
+    this.scoreCounter = document.querySelector("#score-counter");
+
+    this.height = 800;
+    this.width = 700;
+
+    this.ball = new Ball(gameScreen, "./images/ball.png");
+
+    this.refresh;
+    this.refreshRate = Math.floor(1000 / 30); //30 fps
+  }
+
+  shoot(direction) {
+    this.ball.move(direction);
+  }
 }
